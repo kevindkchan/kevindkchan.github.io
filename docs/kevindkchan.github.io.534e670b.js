0c5546af12e332abef3a24763b952cf0517552c1
Kevin Chan <140776786+kevindkchan@users.noreply.github.com>
2025-04-01 00:02:10 -0600

    Revert "Create parcel docs"
    This reverts commit 3cceb04e1c51b676dae435852c650a152d656fdf.

--- docs/kevindkchan.github.io.534e670b.js
+++
@@ -1,2 +0,0 @@
-let dateElement=document.getElementById("date"),options={year:"numeric",month:"long",day:"numeric"},today=new Date().toLocaleDateString(void 0,options);dateElement.textContent=today;
-//# sourceMappingURL=kevindkchan.github.io.534e670b.js.map
