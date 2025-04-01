3cceb04e1c51b676dae435852c650a152d656fdf
Kevin Chan <140776786+kevindkchan@users.noreply.github.com>
2025-03-31 23:54:41 -0600

    Create parcel docs

---
+++ docs/kevindkchan.github.io.534e670b.js
@@ -0,0 +1,2 @@
+let dateElement=document.getElementById("date"),options={year:"numeric",month:"long",day:"numeric"},today=new Date().toLocaleDateString(void 0,options);dateElement.textContent=today;
+//# sourceMappingURL=kevindkchan.github.io.534e670b.js.map
---
+++ docs/kevindkchan.github.io.dda54c6f.js
@@ -0,0 +1,2 @@
+let cursor=document.getElementById("cursor");document.addEventListener("mousemove",e=>{cursor.style.top=`${e.clientY}px`,cursor.style.left=`${e.clientX}px`,e.target.closest("a, button, input, textarea, select, label, .toggle")?cursor.classList.add("hover"):cursor.classList.remove("hover")});
+//# sourceMappingURL=kevindkchan.github.io.dda54c6f.js.map
